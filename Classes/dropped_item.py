d8c6c392313ebd44b87721b5296c5a1879a71d2a
Hunny <hanialtoree80@gmail.com>
2022-04-10 14:39:34 +0300

    func check_pick_up added

--- Classes/dropped_item.py
+++ Classes/dropped_item.py
@@ -6,9 +6,17 @@ class Dropped_item:
         self.x = x
         self.y = y
         self.lvl = lvl
+        self.name = name
         self.image = pygame.image.load(f'../Assets/icons/icon-{name}.png')
         self.image = pygame.transform.scale(self.image, (70, 70))
         self.time_dropped = time_dropped
 
     def show_item_on_surface(self, screen):
         screen.blit(self.image, (self.x - 35, self.y - 35))
+
+    def check_pick_up(self, P):
+        item_rect = self.image.get_rect()
+        item_rect.center = self.x, self.y
+        P_rect = pygame.Rect((0, 0), (100, 100))
+        P_rect.center = P.x, P.y
+        return item_rect.colliderect(P_rect)
